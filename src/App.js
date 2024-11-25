1f897da4d878044e077bc59687a1ffabd92f2b4f
Alexsey Sinyakin <lexavandal@mail.ru>
2024-11-25 19:18:38 +0300

    blog

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+    text-align: center;
+`;
+
+export const App = () => {
+    return (
+        <Div>
+            <i className="fa fa-eye-slash "></i>
+            <div>123</div>
+        </Div>
+    );
+};
